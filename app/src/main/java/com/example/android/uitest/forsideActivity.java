6f3bd8c1991201ab99a5cbf8d04d751be2c25e21
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2020-05-15 14:30:21 +0200

    REEEE

--- app/src/main/java/com/example/android/uitest/forsideActivity.java
+++ app/src/main/java/com/example/android/uitest/forsideActivity.java
@@ -2,7 +2,9 @@ package com.example.android.uitest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class forsideActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class forsideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forside);
+
+        final Button button;
+
+    }
+
+    private void openChat(){
+
+        Intent intent = new Intent(this, klientChat.class);
+        startActivity(intent);
+
+    }
+
+    private void openOevelse(){
+
+        Intent intent = new Intent(this, oevelseActivity.class);
+        startActivity(intent);
+
     }
 }
